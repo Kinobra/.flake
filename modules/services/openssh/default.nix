abad9e3d9edf38d5d26ae70d74de9e9bf93aa387
sntx <sntx.htqjd@simplelogin.com>
2023-05-14 01:02:43 +0200

    Updated ssh keys

--- modules/services/openssh/default.nix
+++ modules/services/openssh/default.nix
@@ -13,16 +13,16 @@ in {
 
   config = mkIf cfg.enable {
 
-    networking.firewall.allowedTCPPorts = [ 22 ];
     services.openssh = {
       enable = true;
       settings = {
-        permitRootLogin = "yes";
+        # permitRootLogin = "no";
         passwordAuthentication = false;
       };
     };
     users.users."${config.user.name}".openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFCcOm5bv/HZtyaavJ0xBFvZJ6fLfuUxhtFj1UU7YXfi" # nixos
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEx240I+HTAgTFYqyUzQr4vn2TR4wr1XAGT1h2mf0vZg sntx.htqjd@simplelogin.com" # minerva
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH5sKJwoWWgrslkvtCMARjtY/IM9V7HHVX3HgsSvhqP0 sntx.htqjd@simplelogin.com" # nixos
     ];
   };
 }

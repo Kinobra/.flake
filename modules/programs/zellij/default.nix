1d54777608446fcc49c1da56952faa2bc31a5cc8
sntx <sntx.htqjd@simplelogin.com>
2023-05-26 13:16:22 +0200

    Added theme to zellij

--- modules/programs/zellij/default.nix
+++ modules/programs/zellij/default.nix
@@ -14,6 +14,24 @@ in {
   config = mkIf cfg.enable {
     home.programs.zellij = {
       enable = true;
+      settings = let
+        colors = config.theme.colors;
+      in {
+        theme = "${config.theme.name}";
+        themes."${config.theme.name}" = {
+          fg      = "${colors.primary.foreground}";
+          bg      = "${colors.primary.background}";
+          black   = "${colors.normal.black}";
+          red     = "${colors.normal.red}";
+          green   = "${colors.normal.green}";
+          yellow  = "${colors.normal.yellow}";
+          blue    = "${colors.normal.blue}";
+          magenta = "${colors.normal.magenta}";
+          cyan    = "${colors.normal.cyan}";
+          white   = "${colors.normal.white}";
+          orange  = "#D08770";
+        };
+      };
     };
   };
 }

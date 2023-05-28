9b8d42b3b34c1d1b48e11e641e1cd76a4899c4b8
sntx <sntx.htqjd@simplelogin.com>
2023-05-28 09:58:47 +0200

    Kitty now follow the set theme

--- modules/programs/kitty/default.nix
+++ modules/programs/kitty/default.nix
@@ -19,7 +19,9 @@ in {
         package = pkgs.fira-code;
         size = 11;
       };
-      settings = {
+      settings = let
+        colors = config.theme.colors;
+      in {
         # cursor
         cursor_shape = "block";
         # mouse
@@ -29,25 +31,25 @@ in {
         hide_window_decorations = "yes";
         confirm_os_window_close = "0";
         # color scheme
-        foreground = "#f8f8f8";
-        background = "#000000";
-        background_opacity = "0.86";
-        color0  = "#241f31"; # black
-        color8  = "#5e5c64"; # black (bright)
-        color1  = "#a51d2d"; # red
-        color9  = "#ed333b"; # red (bright)
-        color2  = "#26a269"; # green
-        color10 = "#57e389"; # green (bright)
-        color3  = "#e66100"; # yellow
-        color11 = "#ff7800"; # yellow (bright)
-        color4  = "#1a5fb4"; # blue
-        color12 = "#3584e4"; # blue (bright)
-        color5  = "#613583"; # magenta
-        color13 = "#c061cb"; # magenta (bright)
-        color6  = "#e5a50a"; # cyan
-        color14 = "#f6d32d"; # cyan (bright)
-        color7  = "#9a9996"; # white
-        color15 = "#ffffff"; # white (bright)
+        foreground = "${colors.primary.foreground}";
+        background = "${colors.primary.background}";
+        background_opacity = toString config.theme.opacity;
+        color0  = "${colors.normal.black}"; # black
+        color1  = "${colors.normal.red}"; # red
+        color2  = "${colors.normal.green}"; # green
+        color3  = "${colors.normal.yellow}"; # yellow
+        color4  = "${colors.normal.blue}"; # blue
+        color5  = "${colors.normal.magenta}"; # magenta
+        color6  = "${colors.normal.cyan}"; # cyan
+        color7  = "${colors.normal.white}"; # white
+        color8  = "${colors.bright.black}"; # black (bright)
+        color9  = "${colors.bright.red}"; # red (bright)
+        color10 = "${colors.bright.green}"; # green (bright)
+        color11 = "${colors.bright.yellow}"; # yellow (bright)
+        color12 = "${colors.bright.blue}"; # blue (bright)
+        color13 = "${colors.bright.magenta}"; # magenta (bright)
+        color14 = "${colors.bright.cyan}"; # cyan (bright)
+        color15 = "${colors.bright.white}"; # white (bright)
       };
     };
 

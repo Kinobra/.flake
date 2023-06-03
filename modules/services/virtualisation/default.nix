20c3ae3edb580e3f2da85bbd4149d3462afea941
sntx <sntx.htqjd@simplelogin.com>
2023-06-03 22:49:50 +0200

    Added polkit to virtualisation

--- modules/services/virtualisation/default.nix
+++ modules/services/virtualisation/default.nix
@@ -12,7 +12,22 @@ in {
   };
 
   config = mkIf cfg.enable {
-
+    security.polkit.enable = true;
+    systemd = {
+      user.services.polkit-gnome-authentication-agent-1 = {
+        description = "polkit-gnome-authentication-agent-1";
+        wantedBy = [ "graphical-session.target" ];
+        wants = [ "graphical-session.target" ];
+        after = [ "graphical-session.target" ];
+        serviceConfig = {
+            Type = "simple";
+            ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+            Restart = "on-failure";
+            RestartSec = 1;
+            TimeoutStopSec = 10;
+          };
+      };
+    };
     users.groups.nixosvmtest = {};
     users.users.vm = {
       isSystemUser = true;
@@ -37,7 +52,7 @@ in {
       virt-manager
     ];
 
-    # users.users."${config.user.name}".extraGroups = [ "libvirtd" ];
+    users.users."${config.user.name}".extraGroups = [ "libvirtd" ];
     virtualisation.libvirtd.enable = true;
     # home.dconf.enable = true;
     programs.dconf.enable = true;

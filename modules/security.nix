d0bb8926dc9928592c8c916ca586e221ee5ebd13
sntx <sntx.htqjd@simplelogin.com>
2023-06-08 11:11:49 +0200

    Fixed Steam NM popup

--- modules/security.nix
+++ modules/security.nix
@@ -24,6 +24,13 @@
   enable = true;
   extraConfig = ''
     polkit.addRule(function(action, subject) {
+      if (action.id === "org.freedesktop.NetworkManager.settings.modify.system") {
+        var name = polkit.spawn(["cat", "/proc/" + subject.pid + "/comm"]);
+        if (name.includes("steam")) {
+          polkit.log("ignoring steam NM prompt");
+          return polkit.Result.NO;
+        }
+      }
       if (action.id.indexOf("org.freedesktop.NetworkManager.") == 0 && subject.isInGroup("network")) {
         return polkit.Result.YES;
       }

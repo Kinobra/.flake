4019ad66a058f09f81736a59dacc6f1e6a68c966
kinobra <116380629+Kinobra@users.noreply.github.com>
2023-06-28 13:54:09 +0200

    kde

---
+++ modules/desktops/kde/default.nix
@@ -0,0 +1,32 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+let cfg = config.myDesktops.kde;
+
+in
+{
+  options.myDesktops.kde = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+  };
+};
+config = mkIf cfg.enable {
+	myPrograms = {
+};
+  	services.xserver.enable = true;
+  	services.xserver.displayManager.sddm.enable = true;
+  	services.xserver.desktopManager.plasma5.enable = true;
+
+    environment.plasma5.excludePackages = with pkgs.libsForQt5; [
+  	elisa
+  	gwenview
+  	okular
+  	oxygen
+  	khelpcenter
+  	konsole
+  	plasma-browser-integration
+  	print-manager
+	];        
+      };
+    }
